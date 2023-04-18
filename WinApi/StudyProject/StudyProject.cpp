6fb6ecaa3a98d78127dbb6392f7483e23094edd7
Will-Big <dlatkdgkr1@gmail.com>
2023-04-19 01:15:37 +0900

    Change Func: MyRegisterClass
    make lpszMenuName = nullptr

--- WinApi/StudyProject/StudyProject.cpp
+++ WinApi/StudyProject/StudyProject.cpp
@@ -76,7 +76,8 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_STUDYPROJECT));
 	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
 	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_STUDYPROJECT);
+	//wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_STUDYPROJECT);
+	wcex.lpszMenuName = nullptr;
 	wcex.lpszClassName = szWindowClass;
 	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
