fa629fe0c4f5a281375b1e6badbe2be225ebdeae
Will-Big <dlatkdgkr1@gmail.com>
2023-04-21 20:39:13 +0900

    12, Double Buffering

--- WinApi/StudyProject/CCore.cpp
+++ WinApi/StudyProject/CCore.cpp
@@ -19,6 +19,14 @@ int CCore::init(HWND _hWnd, POINT _ptResolution)
 
 	m_hDC = GetDC(m_hWnd);
 
+	// 이중 버퍼링 용도의 비트맵과 DC 를 만든다.
+	m_hBit = CreateCompatibleBitmap(m_hDC, m_ptResolution.x, m_ptResolution.y);
+	m_memDC = CreateCompatibleDC(m_hDC);
+
+	// DC 가 디폴트로 가지고 있는 1픽셀의 비트맵을 반환(사용하지 않으므로 삭제)
+	HBITMAP hOldBit = (HBITMAP)SelectObject(m_memDC, m_hBit);
+	DeleteObject(hOldBit);
+
 	// Manager 초기화
 	CTimeMgr::GetInst()->init();
 	CKeyMgr::GetInst()->init();
@@ -58,20 +66,29 @@ void CCore::update()
 
 void CCore::render()
 {
+	// 화면 Clear
+	Rectangle(m_memDC, -1, -1, m_ptResolution.x + 1, m_ptResolution.y + 1);
+
+	// 그리기
 	Vec2 vPos = g_obj.GetPos();
 	Vec2 vScale = g_obj.GetScale();
 
-	Rectangle(m_hDC
+	Rectangle(m_memDC
 		, int(vPos.x - vScale.x / 2.f)
 		, int(vPos.y - vScale.y / 2.f)
 		, int(vPos.x + vScale.x / 2.f)
 		, int(vPos.y + vScale.y / 2.f));
+
+	// 프레임 드랍을 많이 일으키는 작업
+	BitBlt(m_hDC, 0, 0, m_ptResolution.x, m_ptResolution.y, m_memDC, 0, 0, SRCCOPY);
 }
 
 CCore::CCore()
 	: m_hWnd(0)
 	, m_ptResolution{}
 	, m_hDC(0)
+	, m_hBit(0)
+	, m_memDC(0)
 {
 
 }
@@ -79,4 +96,8 @@ CCore::CCore()
 CCore::~CCore()
 {
 	ReleaseDC(m_hWnd, m_hDC);
+
+	// CreateCompatible 으로 생성된 DC 는 아래와 같이 해제해야함
+	DeleteDC(m_memDC);
+	DeleteObject(m_hBit);
 }
